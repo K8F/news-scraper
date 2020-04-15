0241c44205156b025a96c08b47f52e91b122f709
Kate Foust <kate.e.foust@gmail.com>
2020-04-15 11:42:28 -0700

    server runs html routes

--- server.js
+++ server.js
@@ -1,38 +1,44 @@
 var express = require("express");
-var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
-var db = require("./models");
+
 
 var PORT = process.env.PORT || 3000;
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
+// Initialize Express
 var app = express();
 
+// Configure middleware
+
+// Use morgan logger for logging requests
 app.use(logger("dev"));
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
 app.use(express.static("public"));
 
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI, {
-  // useMongoClient: true
-});
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
 
-// db.Headline.create();
-// db.Note.create();
+// Connect to the Mongo DB
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-require("./routes/index.js")(app)
+// Handlebars
+app.engine(
+    "handlebars",
+    exphbs({
+      defaultLayout: "main"
+    })
+  );
+  app.set("view engine", "handlebars");
 
-// additional libraries
-var exphbs = require("express-handlebars");
+// Routes
+//require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
+// Start the server
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
 });
